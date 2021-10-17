82439ec60571e17f2760cbb3469b7d05bf5773c3
Hoàng Quân <92316495+quanhd1811@users.noreply.github.com>
2021-10-17 11:04:53 +0700

    Update FirebaseInitialization.java

--- helloworld-springboot-tomcat/src/main/java/com/example/helloworld/firebase/FirebaseInitialization.java
+++ helloworld-springboot-tomcat/src/main/java/com/example/helloworld/firebase/FirebaseInitialization.java
@@ -3,6 +3,8 @@ package com.example.helloworld.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -12,10 +14,11 @@ import java.io.FileInputStream;
 public class FirebaseInitialization {
     @PostConstruct
     public void initialization() {
+//        ClassLoader classLoader = getClass().getClassLoader();
         Resource resource = new ClassPathResource("serviceAccountKey.json");
         FileInputStream serviceAccount = null;
         try {
-            
+            //classLoader.getResouces("serviceAccountKey.json).getFile
             serviceAccount = new FileInputStream(resource.getFile());
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
